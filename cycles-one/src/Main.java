d4d8863c14c3974f5d72d6aea6f1b37d83d1209e
Anatoly Sorokin <113344170+asorokin2107@users.noreply.github.com>
2022-09-27 18:40:00 +0300

    Add files via upload

---
+++ cycles-one/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 200; i <= 211; i++) {
+            if ( i == 211)
+                continue;
+
+            System.out.println(i);
+        }
+
+        for (int i = 220; i < 251 ; i++) {
+
+            System.out.println(i);
+
+        }
+    }
+}
